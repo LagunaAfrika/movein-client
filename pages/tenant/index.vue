2f110eb0fbbc68fef4198e3c08890ca24a2c1462
Thato Rammoko <thatorammoko@gmail.com>
2020-07-23 11:28:15 +0000

    fixed linting

--- pages/tenant/index.vue
+++ pages/tenant/index.vue
@@ -1,7 +1,16 @@
 <template>
-<v-container>
-    
-</v-container>
+  <v-flex xs12 md12>
+    Full Name
+    <v-text-field
+      id="full_name"
+      v-model="userData.full_name"
+      outlined
+      label="FullName"
+      :rules="nameRules"
+      required
+      color="#0091ad"
+    />
+  </v-flex>
 </template>
 <script>
 export default {
