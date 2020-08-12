eddd1a447af078e3cf3dc48b1d648dd023a52b88
lawrence-chege <lawrence.chege@meltwater.org>
2020-08-12 11:56:16 +0000

    adjust price

--- pages/tenant/price/index.vue
+++ pages/tenant/price/index.vue
@@ -1,30 +1,34 @@
 <template>
   <v-layout justify-center align-center column class="txt">
     <v-flex xs12 md12 class="size mt-4">
-        <v-img class="image" src="/cash.svg" />
-      </v-flex>
+      <v-img class="image" src="/cash.svg" />
+    </v-flex>
 
-      <v-card flat color="transparent">
-        <v-card-title class="text-center">Maximum Budget (KSH)</v-card-title>
-        <v-card-text>
-          <v-slider v-model="slider" class="align-center" :max="max" :min="min" hide-details>
-            <template v-slot:append>
-              <v-text-field
-                v-model="slider"
-                class="mt-0 pt-0"
-                hide-details
-                single-line
-                type="number"
-                style="width: 60px"
-              ></v-text-field>
-            </template>
-          </v-slider>
-        </v-card-text>
-      </v-card>
+    <v-card flat color="transparent">
+      <v-card-title class="text-center">
+        Maximum Budget (KSH)
+      </v-card-title>
+      <v-card-text>
+        <v-slider v-model="slider" class="align-center" :max="max" :min="min" hide-details>
+          <template v-slot:append>
+            <v-text-field
+              v-model="slider"
+              class="mt-0 pt-0"
+              hide-details
+              single-line
+              type="number"
+              style="width: 60px"
+            />
+          </template>
+        </v-slider>
+      </v-card-text>
+    </v-card>
 
     <v-flex xs12 md12>
       <v-card-actions>
-        <v-btn color="yellow darken-4 white--text" class="btn" @click="next">next</v-btn>
+        <v-btn color="yellow darken-4 white--text" class="btn" @click="next">
+          next
+        </v-btn>
       </v-card-actions>
     </v-flex>
   </v-layout>
@@ -35,19 +39,19 @@ export default {
     return {
       isLoading: false,
       tab: null,
-      min: 0,
-      max: 3000000,
+      min: 2500,
+      max: 50000,
 
-      slider: ""
-    };
+      slider: ''
+    }
   },
   methods: {
-    next() {
-      this.$store.commit("SET_RENT", this.slider);
-      this.$router.push("/tenant/bed_bath");
+    next () {
+      this.$store.commit('SET_RENT', this.slider)
+      this.$router.push('/tenant/bed_bath')
     }
   }
-};
+}
 </script>
 <style scoped>
 .next {
