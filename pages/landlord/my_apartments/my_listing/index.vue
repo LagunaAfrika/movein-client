fe2c41671e0670f5fd9afe77e18ee58e5ed7f61e
lawrence-chege <lawrence.chege@meltwater.org>
2020-08-12 01:10:35 +0000

    detach listin button

--- pages/landlord/my_apartments/my_listing/index.vue
+++ pages/landlord/my_apartments/my_listing/index.vue
@@ -1,92 +1,98 @@
 <template>
-  <v-card
-    :loading="loading"
-    class="mx-auto my-12 txt"
-    max-width="374"
-  >
-    <v-img
-      class="white--text align-end"
-      height="200px"
-      :src="getProperty.property_picture"
+  <div>
+    <v-card
+      :loading="loading"
+      class="mx-auto my-12 txt"
+      max-width="374"
     >
-      <v-card-title>Ksh {{ Number(getProperty.details.rent_amount).toLocaleString() }}</v-card-title>
-    </v-img>
-
-    <v-card-text>
-      <v-row
-        align="center"
-        class="mx-0 "
+      <v-img
+        class="white--text align-end"
+        height="200px"
+        :src="getProperty.property_picture"
       >
-        <div class="txt mb-2  black--text">
-          <v-icon color="#ec7d10">
-            mdi-map-marker
-          </v-icon> {{ getProperty.location.name }}
-        </div>
+        <v-card-title>Ksh {{ Number(getProperty.details.rent_amount).toLocaleString() }}</v-card-title>
+      </v-img>
 
-        <div class="yellow--text text--darken-4 ml-4">
-          <v-icon class="yellow--text text--darken-4">
-            mdi-bed
-          </v-icon> {{ getProperty.details.house_type }} <v-icon class="yellow--text text--darken-4">
-            mdi-shower
-          </v-icon> {{ getProperty.details.bathrooms }} {{ getProperty.details.bathrooms > 1 ? 'Bathrooms' : 'Bathroom' }}
-        </div>
-      </v-row>
-    </v-card-text>
+      <v-card-text>
+        <v-row
+          align="center"
+          class="mx-0 "
+        >
+          <div class="txt mb-2  black--text">
+            <v-icon color="#ec7d10">
+              mdi-map-marker
+            </v-icon> {{ getProperty.location.name }}
+          </div>
 
-    <v-divider class="mx-4" />
+          <div class="yellow--text text--darken-4 ml-4">
+            <v-icon class="yellow--text text--darken-4">
+              mdi-bed
+            </v-icon> {{ getProperty.details.house_type }} <v-icon class="yellow--text text--darken-4">
+              mdi-shower
+            </v-icon> {{ getProperty.details.bathrooms }} {{ getProperty.details.bathrooms > 1 ? 'Bathrooms' : 'Bathroom' }}
+          </div>
+        </v-row>
+      </v-card-text>
 
-    <v-card-title class="black--text">
-      Description
-    </v-card-title>
+      <v-divider class="mx-4" />
 
-    <v-card-text>
-      <div>{{ getProperty.details.description }}</div>
-    </v-card-text>
-    <v-divider class="mx-4" />
-    <v-card-title class="black--text mb-2">
-      Basic details
-    </v-card-title>
-    <v-card-text class="black--text">
-      Bedroom
-    </v-card-text>
-    <v-card-text v-for="item in getProperty.details.bedroom" :key="item">
-      {{ item }}
-    </v-card-text>
+      <v-card-title class="black--text">
+        Description
+      </v-card-title>
 
-    <v-divider class="mx-4" />
-    <v-card-text class="black--text">
-      Kitchen
-    </v-card-text>
-    <v-card-text v-for="item in getProperty.details.kitchen" :key="item">
-      {{ item }}
-    </v-card-text>
-    <v-divider class="mx-4" />
-    <v-card-text class="black--text">
-      Living Room
-    </v-card-text>
-    <v-card-text v-for="item in getProperty.details.livingRoom" :key="item">
-      {{ item }}
-    </v-card-text>
+      <v-card-text>
+        <div>{{ getProperty.details.description }}</div>
+      </v-card-text>
+      <v-divider class="mx-4" />
+      <v-card-text class="black--text">
+        Basic house details
+      </v-card-text>
+      <v-card-text v-for="item in getProperty.details.house" :key="item">
+        {{ item }}
+      </v-card-text>
+      <v-divider class="mx-4" />
+      <v-card-text class="black--text">
+        Bedroom Details
+      </v-card-text>
+      <v-card-text v-for="item in getProperty.details.bedroom" :key="item">
+        {{ item }}
+      </v-card-text>
 
-    <v-divider class="mx-4" />
-    <v-card-text class="black--text">
-      Washroom
-    </v-card-text>
-    <v-card-text v-for="item in getProperty.details.washRoom" :key="item">
-      {{ item }}
-    </v-card-text>
+      <v-divider class="mx-4" />
+      <v-card-text class="black--text">
+        Kitchen Details
+      </v-card-text>
+      <v-card-text v-for="item in getProperty.details.kitchen" :key="item">
+        {{ item }}
+      </v-card-text>
+      <v-divider class="mx-4" />
+      <v-card-text class="black--text">
+        Living Room Details
+      </v-card-text>
+      <v-card-text v-for="item in getProperty.details.livingRoom" :key="item">
+        {{ item }}
+      </v-card-text>
 
+      <v-divider class="mx-4" />
+      <v-card-text class="black--text">
+        Washroom Details
+      </v-card-text>
+      <v-card-text v-for="item in getProperty.details.washRoom" :key="item">
+        {{ item }}
+      </v-card-text>
+    </v-card>
     <v-card-actions>
       <v-spacer />
       <v-btn
         color="yellow darken-4"
+        class="bottom"
 
         to="/landlord/add_property"
       >
-        New Apartment
+        New Property
       </v-btn>
     </v-card-actions>
-  </v-card>
+  </div>
 </template>
 
 <script>
@@ -116,5 +122,11 @@ export default {
 <style scoped>
 .txt {
   font-family: "Comfortaa", cursive;
+}
+.bottom{
+  position: absolute;
+  top:90%;
+  left: 37%;
+  right: 37%;
 }
 </style>
