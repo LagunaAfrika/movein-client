87c5b1b2925ab96ffe242e43d89a0fb988bac331
Thato Rammoko <thatorammoko@gmail.com>
2020-08-13 14:08:49 +0000

    just pulled

--- pages/landlord/my_apartments/rent_amount/index.vue
+++ pages/landlord/my_apartments/rent_amount/index.vue
@@ -1,18 +1,16 @@
 <template>
-  <v-container fill-height fluid >
+  <v-container fill-height fluid>
     <v-layout justify-center align-center column>
-           
-            
-            <v-flex xs12 md12 text-xs-right>
-              <v-btn
-                @click="next"
-                class="mx-0 white--text txt "
-                
-                color="#ec7d10"
-              >
-                Next
-              </v-btn>
-            </v-flex>
+      <v-flex xs12 md12 text-xs-right>
+        <v-btn
+          class="mx-0 white--text txt "
+          color="#ec7d10"
+
+          @click="next"
+        >
+          Next
+        </v-btn>
+      </v-flex>
     </v-layout>
   </v-container>
 </template>
@@ -30,9 +28,9 @@ export default {
     }
   },
   methods: {
-     next(){
+    next () {
       this.$store.commit('SET_RENTAMOUNT_DETAILS', this.houseData.rent_amount)
-      this.$router.push("/landlord/my_apartments/my_listing")
+      this.$router.push('/landlord/my_apartments/my_listing')
     }
   }
 }
